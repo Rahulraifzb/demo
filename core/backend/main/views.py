9a0f7facde62a068cfd3d9ed7e129dc018d93dd1
Rahul rai <46862628+Rahulraifzb@users.noreply.github.com>
2021-09-05 15:14:43 +0530

    Update views.py

--- core/backend/main/views.py
+++ core/backend/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return redirect("quiz-list")
+    return HttpResponse("quiz-list")
